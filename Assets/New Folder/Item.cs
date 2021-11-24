cc63a745bdce142ba1ef3c4319289e766b299a20
f0jzd <tomassavela@gmail.com>
2021-11-24 18:13:19 +0100

    Ouga

--- Assets/New Folder/Item.cs
+++ Assets/New Folder/Item.cs
@@ -5,8 +5,33 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    private GameObject item;
-    private String itemName;
-    private bool stackable;
-    private float value;
+    [SerializeField]private GameObject item;
+    [SerializeField]private String itemName;
+    [SerializeField]private bool stackable;
+    [SerializeField]private float value;
+
+
+    public GameObject Item1
+    {
+        get => item;
+        set => item = value;
+    }
+
+    public string ItemName
+    {
+        get => itemName;
+        set => itemName = value;
+    }
+
+    public bool Stackable
+    {
+        get => stackable;
+        set => stackable = value;
+    }
+
+    public float Value
+    {
+        get => value;
+        set => this.value = value;
+    }
 }
--- Assets/New Folder/Player.cs
+++ Assets/New Folder/Player.cs
@@ -24,10 +24,26 @@ public class Player : MonoBehaviour
             return instance;
         }
     }
-
     public int PlayerHealth => playerHealth;
-
     private static Player instance;
+
+
+    private enum State {
+        Walking,
+        Running,
+        Idle,
+    }
+    //Edges, what does the player do basically
+    private enum Event {
+        StoppedMoving,
+        StartedWalking,
+        StartedRunning,
+        StartIdle
+    }
+    
+    private State _pState = State.Idle;
+    private Event _pEvent = Event.StartIdle;
+    
 
     private void Awake()
     {
@@ -36,6 +52,86 @@ public class Player : MonoBehaviour
         } else {
             instance = this;
             DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+        {
+            _pEvent = Event.StartedWalking; 
+            OnEvent(_pEvent);
+        }
+        if (Input.GetButton("Horizontal") && Input.GetKey(KeyCode.LeftShift))
+        {
+            _pEvent = Event.StartedRunning; 
+            OnEvent(_pEvent);
+        }
+        if (!Input.GetButton("Horizontal"))
+        {
+            _pEvent = Event.StoppedMoving;
+            OnEvent(_pEvent);
+        }
+        if (_pState == State.Idle)
+        {
+            Debug.Log("Idle");
+        }
+        if (_pState == State.Walking)
+        {
+            Debug.Log("Walking");
+        }
+        if (_pState == State.Running)
+        {
+            Debug.Log("Running");
+        }
+    }
+
+    private void OnEvent(Event playerEvent)
+    {
+        ///This is literally a switch statement
+        /* if (playerEvent == Event.StartedWalking)
+         {
+             pState = State.Walking;
+         }
+         else if (playerEvent == Event.StartedRunning)
+         {
+                 
+         }*/
+        
+        switch (playerEvent)
+        { 
+            case Event.StartedWalking:
+                switch (_pState)
+                {
+                    case State.Idle:
+                    case State.Running:
+                        _pState = State.Walking;
+                        break;
+                    case State.Walking:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                break;
+            case Event.StartedRunning:
+                _pState = _pState switch
+                {
+                    State.Walking => State.Running,
+                    _ => _pState
+                };
+                break;
+            case Event.StoppedMoving:
+                switch (_pState)
+                {
+                    case State.Walking:
+                    case State.Running: 
+                        _pState = State.Idle;
+                        break;
+                    case State.Idle:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                break;
         }
     }
     
